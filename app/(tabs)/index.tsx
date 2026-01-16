44e719a5e09ad757c24333d8692b21d0d8cb0aeb
salavat <nicecube11@yandex.ru>
2026-01-16 16:37:57 +0300

    fix

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {Link} from 'expo-router'
+
+export default function index() {
+  return (
+    <View>
+      <Text>index</Text>
+      <Link href="/about">Go to about screen</Link>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
